e863b5f3fa4d2c815ddb59a54a405543c380b17f
HongBeen Lee <65804460+Hong-been@users.noreply.github.com>
2021-07-28 17:57:12 +0900

    Create app.py : 나홀로메모장 서버 파일 생성
    - 클라이언트와 통신하여 데이터 저장, 저장된 데이터 꺼내오기 기능
    - 영화의 url과 코멘트를 입력하면, url의 메타데이터(제목, 포스터이미지, 영화설명, url)와 코멘트를 서버에 저장
    - 저장된 모든 데이터를 카드형식으로 브라우저에 보여준다.

---
+++ alonelemo/app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, jsonify, request
+app = Flask(__name__)
+
+import requests
+from bs4 import BeautifulSoup
+
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
+
+## HTML을 주는 부분
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+@app.route('/memo', methods=['GET'])
+def listing():
+    articles = list(db.articles.find({}, {'_id': False}))
+    return jsonify({'all_articles':articles})
+
+## API 역할을 하는 부분
+@app.route('/memo', methods=['POST'])
+def saving():
+    url_receive = request.form['url_give']
+    comment_receive = request.form['comment_give']
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url_receive, headers=headers)
+
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    title = soup.select_one('meta[property="og:title"]')['content']
+    desc = soup.select_one('meta[property="og:description"]')['content']
+    image = soup.select_one('meta[property="og:image"]')['content']
+
+    doc={
+        'title':title,
+        'image': image,
+        'desc': desc,
+        'url':url_receive,
+        'comment':comment_receive
+    }
+
+    db.articles.insert_one(doc)
+
+    return jsonify({'msg':'Complete to save!'})
+
+if __name__ == '__main__':
+   app.run('0.0.0.0',port=5000,debug=True)
