1aeaec79453d2c7d4dee21a9505627c0cecbe734
HongBeen Lee <65804460+Hong-been@users.noreply.github.com>
2021-07-28 19:50:08 +0900

    Create app.py : 원페이지 쇼핑몰 서버 파일 생성
    - 사용자가 입력한 데이터(이름, 수량, 주소, 전화번호)를 서버에 저장한다.
    - 새로고침 될때마다, 페이지 하단에 저장된 모든 데이터를 보여준다.

---
+++ shoppingmall/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client.dbhomework
+
+## HTML 화면 보여주기
+@app.route('/')
+def homework():
+    return render_template('index.html')
+
+
+# 주문하기(POST) API
+@app.route('/order', methods=['POST'])
+def save_order():
+    name_receive = request.form['name_give']
+    quantity_receive = request.form['quantity_give']
+    address_receive = request.form['address_give']
+    phone_receive = request.form['phone_give']
+
+    doc={
+        'name':name_receive,
+        'quantity':quantity_receive,
+        'address':address_receive,
+        'phone':phone_receive
+    }
+
+    db.shop.insert_one(doc)
+
+    return jsonify({'msg': 'Your order is completed. Thank you!'})
+
+
+# 주문 목록보기(Read) API
+@app.route('/order', methods=['GET'])
+def view_orders():
+    orders = list(db.shop.find({},{'_id':False}))
+    return jsonify({'all_orders': orders})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
