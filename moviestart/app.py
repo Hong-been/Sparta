24250b0a09d860cab0cb15b0c764f40f19375140
HongBeen Lee <65804460+Hong-been@users.noreply.github.com>
2021-07-29 23:12:33 +0900

    Create app.py : 영화배우 리스트를 좋아요 순으로 보여주는 서버 파일 생성
    - 영화배우의 사진, 이름, 최신작, 좋아요 버튼, 리스트에서 삭제 버튼
    - 항상 좋아요가 많은 순으로 서버에서 가져온다.
    - 좋아요 버튼을 누르면 서버에 전달하여 +1 증가한 값을 저장한다.

---
+++ moviestart/app.py
@@ -0,0 +1,40 @@
+from pymongo import MongoClient
+
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
+
+
+# HTML 화면 보여주기
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+# API 역할을 하는 부분
+@app.route('/api/list', methods=['GET'])
+def show_stars():
+    movie_stars = list(db.mystar.find({}, {'_id':False}).sort("like", -1))
+    return jsonify({'all_stars': movie_stars})
+
+
+@app.route('/api/like', methods=['POST'])
+def like_star():
+    name_receive = request.form['name_give']
+    target_star = db.mystar.find_one({'name': name_receive})
+    new_like = target_star['like'] + 1
+    db.mystar.update_one({'name': name_receive}, {'$set': {'like': new_like}})
+    return jsonify({'msg': '좋아요 되었습니다!'})
+
+@app.route('/api/delete', methods=['POST'])
+def delete_star():
+    name_receive = request.form['name_give']
+    target_star = db.mystar.find_one({'name': name_receive})
+    db.mystar.delete_one({'name': name_receive})
+    return jsonify({'msg': '삭제되었습니다!'})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
